6c07370df8dbaf492bd967ef9eb872ba2928bdf8
vitoriatedeia <viih.tedeia@gmail.com>
2025-04-04 12:10:41 -0300

    Comentários

--- Atividade-04-04/script/script.js
+++ Atividade-04-04/script/script.js
@@ -1,14 +1,18 @@
+// Classe Funcionario, que representa um funcionário
 class Funcionario {
+  // Construtor da classe Funcionario, recebe nome, salario e data de admissão
   constructor(nome, salario, dataAdmissao) {
-    this.nome = nome;
-    this.salario = salario;
-    this.dataAdmissao = dataAdmissao;
+    this.nome = nome; // Atribui o nome do funcionário
+    this.salario = salario; // Atribui o salário do funcionário
+    this.dataAdmissao = dataAdmissao; // Atribui a data de admissão
   }
 
+  // Método que calcula o bônus do funcionário (10% do salário)
   calcularBonus() {
-    return this.salario * 0.1;
+    return this.salario * 0.1; // Retorna 10% do salário
   }
 
+  // Método que exibe os detalhes do funcionário
   exibirDetalhes() {
     console.log(
       `O funcionário ${this.nome} foi admitido em ${
@@ -20,16 +24,20 @@ class Funcionario {
   }
 }
 
+// Classe Gerente, que herda de Funcionario
 class Gerente extends Funcionario {
+  // Construtor da classe Gerente, que também recebe nome, salário, data de admissão e um novo atributo: departamento
   constructor(nome, salario, dataAdmissao, departamento) {
-    super(nome, salario, dataAdmissao);
-    this.departamento = departamento;
+    super(nome, salario, dataAdmissao); // Chama o construtor da classe pai (Funcionario)
+    this.departamento = departamento; // Atribui o departamento do gerente
   }
 
+  // Sobrescrita do método calcularBonus para a classe Gerente (bônus de 20% do salário)
   calcularBonus() {
-    return this.salario * 0.2;
+    return this.salario * 0.2; // Retorna 20% do salário
   }
 
+  // Método específico para exibir os detalhes do gerente
   exibirDetalhesGerente() {
     console.log(
       `A gerente ${this.nome} foi admitida em ${
@@ -41,12 +49,19 @@ class Gerente extends Funcionario {
   }
 }
 
+// Criação de uma instância da classe Gerente
 const gerente = new Gerente(
-  "Vitória Tedeia",
-  "15000",
-  "15/10/2021",
-  "Contabilidade"
+  "Vitória Tedeia", // Nome do gerente
+  "15000", // Salário do gerente
+  "15/10/2021", // Data de admissão
+  "Contabilidade" // Departamento do gerente
 );
-const funcionario = new Funcionario("Kauê Weber", "10000", "15/04/2024");
+
+// Criação de uma instância da classe Funcionario
+const funcionario = new Funcionario("Kauê Weber", "10000", "15/04/2024"); // Nome, salário e data de admissão do funcionário
+
+// Exibe os detalhes do funcionário
 funcionario.exibirDetalhes();
+
+// Exibe os detalhes do gerente
 gerente.exibirDetalhesGerente();
